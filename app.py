ce10d44b5a239da9cd48762ee9b9ffd3db05e9d1
Saman Sarker Joy <samanjoy2@gmail.com>
2023-12-12 22:53:39 +0600

    dsds

--- app.py
+++ app.py
@@ -3,22 +3,17 @@ from transformers import pipeline
 import torch
 
 # Load the text-to-text generation pipeline
-pipe = pipeline("text2text-generation", model="samanjoy2/bnpunct_banglat5_seq2seq_finetuned")
+pipe = pipeline("text2text-generation", model="samanjoy2/bnpunct_banglat5_seq2seq_finetuned", device=0)
 
 st.title("Bangla Punctutation Restoration")
 
 # User input for text generation
-input_text = st.text_area("Enter Bangla text for restoration:", max_chars=500)
-
-def generate_text(input_text):
-    # Use the pipeline to generate text
-    generated_text = pipe(input_text, max_length=512, batch_size=64)[0]['generated_text']
-    return generated_text
+input_text = st.text_area("Enter Bangla text for restoration:", max_chars=450)
 
 if st.button("Restore Punctuations"):
     if input_text:
         # Generate text using the pipeline
-        generated_text = generate_text(input_text)
+        generated_text = pipe(input_text, max_length=512, batch_size=1)[0]['generated_text']
 
         # Display the generated text
         st.subheader("Generated Text:")
